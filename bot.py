cb8e5b68199230ef11d3f0ae8bfb3dc7158e2de5
Alexandre Masssonié <massonie.alexandre@gmail.com>
2019-01-18 21:22:41 +0100

    Introduced permission system and some dev commands

--- bot.py
+++ bot.py
@@ -3,32 +3,70 @@ import asyncio
 import logging
 import sys
 
+
+_MASTERS_IDS_ = ["217982938703331328"]
+
 class Bot(discord.Client):
     async def on_ready(self):
         print("Logged on as {}!".format(self.user))
+
 
     async def is_for_me(self, message):
         return message.content.startswith("./") and (message.author != self.user) 
 
 
+    async def check_rank(self, member, server):
+        if(member == None or server == None):
+            return None
+        
+        if("ltools" not in [i.name for i in server.roles]):
+            try:
+                ltools_role = await self.create_role(server)
+                await self.edit_role(server, ltools_role, name="ltools")
+                await self.add_roles(server.me, ltools_role)
+            except discord.errors.Forbidden:
+                await self.send_message(server.default_channel, "Missing permission : [discord.manage_roles]")
+
+        if("ltools" in [i.name for i in member.roles]):
+            return True
+        return False
+        
+
     async def on_message(self, message):
         if(await self.is_for_me(message)):
-            print("{}@{}:\'{}\'".format(message.author.name, message.server.name, message.content))
-            await self.process_message(message)
-    
+            authorized = await self.check_rank(message.author, message.channel.server)
+            print("{}@{}:\'{}\'{}".format(message.author.name, message.server.name, message.content, authorized))
+            if(authorized):
+                await self.process_message(message)
+            else:
+                await self.delete_message(message)
+            
 
     async def process_message(self, message):
         args = message.content.split(" ")
         args[0] = args[0][2:]   #Removing prefix
+        
+        #Regular commands
         if(args[0] == "ping"):
-            print("ping")
             await self.send_message(message.channel, "pong!")
         elif(args[0] == "pong"):
             await self.send_message(message.channel, "ping!")
         elif(args[0] == "kick"):
             await self.kick_cmd(args, message)
-    
+        
+        #Dev commands
+        if(args[0].startswith("dev") and message.author.id not in _MASTERS_IDS_):
+            await self.send_message(message.channel, "Who do you think you are ?")
+            return
 
+        if(args[0] == "dev/checkrank"):
+            user = await self.find_user(args[1], message.server)
+            res = await self.check_rank(user, message.server)
+            await self.send_message(message.channel, str(res))
+        elif(args[0] == "dev/kill"):
+            await self.die()
+    
+    
     async def kick_cmd(self, args, message):
         if(len(args) <=1 or message.server == None):
             await self.send_message(message.channel, "./kick_cmd (user) [-f(orce)]")
@@ -72,6 +110,10 @@ class Bot(discord.Client):
             if(member.nick == username or member.name == username):
                 return member
         return None
+    
+
+    async def die(self):
+        await self.logout()
 
 def help():
     print("python bot.py {client token} [...]")
